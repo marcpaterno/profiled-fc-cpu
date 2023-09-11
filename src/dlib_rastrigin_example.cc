44256d7f674a68132f60c1c5d705318a15b39a88
Marc Paterno <paterno@fnal.gov>
2023-09-11 11:20:30 -0500

    Add dlib experiments using Rastrigin function

--- src/dlib_rastrigin_example.cc
+++ src/dlib_rastrigin_example.cc
@@ -15,39 +15,82 @@ rastrigin_dlib_wrapper(column_vector const& x)
   return pfc::rastrigin({begin(x), end(x)});
 }
 
+// Structure to supply the boundaries for the minimization search region.
+struct bounds {
+  explicit bounds(int dim) : lower(dim), upper(dim) {}
+  column_vector lower;
+  column_vector upper;
+};
+
+bounds
+make_bounds(int dim)
+{
+  // We intentionally make the bounds non-symmetric, to avoid having the
+  // optimizer take advantage of the symmetry of the rastrigin function.
+  bounds result(dim);
+  for (int i = 0; i < dim; ++i) {
+    result.lower(i) = -19.0 + i;
+    result.upper(i) = 1.0 + i;
+  }
+  return result;
+}
+
 bool
-do_one_minimization(dlib::thread_pool& threads, long maxcalls, std::ostream& os)
+do_one_minimization(column_vector const& lower_bounds,
+                    column_vector const& upper_bounds,
+                    int dim,
+                    long maxcalls,
+                    std::ostream& os)
 {
   static bool first = true;
+  auto start = std::chrono::steady_clock::now();
   dlib::function_evaluation result = dlib::find_min_global(
-    threads,
     rastrigin_dlib_wrapper,
-    {-10.0, -7.0, -13.0},              // lower bounds
-    {5.0, 8.0, 2.0},                   // upper bounds
+    lower_bounds,
+    upper_bounds,
     dlib::max_function_calls(maxcalls) // max function evaluations
   );
-
+  auto stop = std::chrono::steady_clock::now();
+  auto delta_t = stop - start;
   // Print out the header the first time we're called
   if (first) {
-    os << "maxcalls\tfmin\tx0\tx1\tx2\n";
     first = false;
+    os << "maxcalls\tfmin";
+    for (int i = 0; i != dim; ++i) {
+      os << "\tx" << i;
+    }
+    os << "\ttime\n";
   }
   os << maxcalls << '\t' << result.y;
-  for (int i = 0; i < 3; ++i) {
+  for (int i = 0; i != dim; ++i) {
     os << '\t' << result.x(i);
   }
-  os << '\n';
+  os << '\t'
+     << std::chrono::duration_cast<std::chrono::microseconds>(delta_t).count()
+     << '\n';
   return abs(result.y) < 1.0e-6;
 }
 
 int
-main()
+main(int argc, char* argv[])
 {
+  if (argc != 2) {
+    std::cerr << "Please supply the dimensionality to be used\n";
+    return 1;
+  }
+  std::vector<std::string> args(argv + 1, argv + argc);
+  int const dim = std::stoi(args[0]);
+  if ((dim <= 0) or (dim > 20)) {
+    std::cerr << "Please supply a positive dimensionality less than 20\n";
+    return 2;
+  }
+  auto [lower_bounds, upper_bounds] = make_bounds(dim);
+
   std::cout.precision(17);
-  dlib::thread_pool threads(8); // set this based on your hardware
-  for (long i = 0; i < 20; ++i) {
+  for (long i = 0; i != 24; ++i) {
     long maxcalls = std::pow(2, i);
-    bool converged = do_one_minimization(threads, maxcalls, std::cout);
+    bool converged =
+      do_one_minimization(lower_bounds, upper_bounds, dim, maxcalls, std::cout);
     if (converged)
       break;
   }
